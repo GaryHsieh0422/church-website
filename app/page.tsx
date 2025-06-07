b8f1caa866d2ba92055c023c3342f4d36213ca64
Gary <hsiehhungluiyee0422@gmail.com>
2025-06-08 01:30:15 +0800

    added animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,67 @@
+"use client";
+
+import { motion, useInView } from "framer-motion";
 import Hero from "@/components/Hero";
 import ScheduleSection from "@/components/schedule";
 import Map from "@/components/map";
+import { useRef } from "react";
+
 export default function Home() {
+  const simpleFade = {
+    initial: { opacity: 0, y: 10 },
+    animate: { opacity: 1, y: 0 },
+  };
+
+  const heroRef = useRef(null);
+  const scheduleRef = useRef(null);
+  const mapRef = useRef(null);
+
+  // Track when each section is in view, animating only once
+  const heroInView = useInView(heroRef, {
+    once: true,
+    amount: 0.5,
+    margin: "0px 0px -100px 0px",
+  });
+  const scheduleInView = useInView(scheduleRef, {
+    once: true,
+    amount: 0.5,
+    margin: "0px 0px -100px 0px",
+  });
+  const mapInView = useInView(mapRef, {
+    once: true,
+    amount: 0.5,
+    margin: "0px 0px -100px 0px",
+  });
+
   return (
     <main>
-      <Hero></Hero>
-      <ScheduleSection></ScheduleSection>
-      <Map></Map>
+      <motion.section
+        ref={heroRef}
+        variants={simpleFade}
+        initial="initial"
+        animate={heroInView ? "animate" : "initial"}
+        transition={{ duration: 1.0, ease: "easeOut", delay: 0.1}}
+      >
+        <Hero />
+      </motion.section>
+      <motion.section
+        ref={scheduleRef}
+        variants={simpleFade}
+        initial="initial"
+        animate={scheduleInView ? "animate" : "initial"}
+        transition={{ duration: 1.0, ease: "easeOut", delay: 0.1 }}
+      >
+        <ScheduleSection />
+      </motion.section>
+      <motion.section
+        ref={mapRef}
+        variants={simpleFade}
+        initial="initial"
+        animate={mapInView ? "animate" : "initial"}
+        transition={{ duration: 1.0, ease: "easeOut", delay: 0.6 }}
+      >
+        <Map />
+      </motion.section>
     </main>
   );
 }
